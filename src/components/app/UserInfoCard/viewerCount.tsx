5e1609abc2d337e7917656dbee59e6b84ea8bc52
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-03-18 19:24:59 +0100

    fix: viewercount to show actual count

--- src/components/app/UserInfoCard/viewerCount.tsx
+++ src/components/app/UserInfoCard/viewerCount.tsx
@@ -1,12 +1,14 @@
 import { useStreams } from "@/lib/providers/StreamInfoProvider";
 import { User } from "lucide-react";
+import { useParams } from "next/navigation";
 
 export default function ViewerCount() {
   const streamInfo = useStreams();
+  const { username } = useParams();
 
   if (streamInfo.isLoading) return null;
 
-  const viewerCount = streamInfo.stream!.reduce((acc, stream) => acc + stream.viewers, 0);
+  const viewerCount = streamInfo.stream!.find(s => s.username === username)?.viewers;
 
   return (
     <div className="flex items-center space-x-2 *:text-destructive">
