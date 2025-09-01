d656d0f579a29613727573631376ffcd265216fb
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-09-02 00:31:02 +0200

    fix: docs builds

--- apps/docs/src/mdx-components.tsx
+++ apps/docs/src/mdx-components.tsx
@@ -4,6 +4,7 @@ import type { MDXComponents } from 'mdx/types';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
+  // @ts-ignore
   return {
     ...defaultMdxComponents,
     Mermaid,
