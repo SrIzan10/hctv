2298b8bc0c264a4629d8722fc64f4be914aaa42c
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-09-30 17:31:07 +0200

    feat: small amount of documentation

--- apps/web/src/components/app/NavBar/NavBar.tsx
+++ apps/web/src/components/app/NavBar/NavBar.tsx
@@ -54,6 +54,18 @@ export default function Navbar(props: Props) {
                   <Link href={`/settings/channel/create`}>
                     <DropdownMenuItem className="cursor-pointer">Create channel</DropdownMenuItem>
                   </Link>
+                  <Link href={`/settings/bot`}>
+                    <DropdownMenuItem className="cursor-pointer">Bot accounts</DropdownMenuItem>
+                  </Link>
+                  <Link href={'https://docs.hctv.srizan.dev'} target="_blank" rel="noreferrer">
+                    <DropdownMenuItem className="cursor-pointer">API Docs</DropdownMenuItem>
+                  </Link>
+                  <Link href={'https://github.com/SrIzan10/hctv'} target="_blank" rel="noreferrer">
+                    <DropdownMenuItem className="cursor-pointer">GitHub</DropdownMenuItem>
+                  </Link>
+                  <Link href={'https://github.com/sponsors/SrIzan10'} target="_blank" rel="noreferrer">
+                    <DropdownMenuItem className="cursor-pointer">Sponsor</DropdownMenuItem>
+                  </Link>
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
