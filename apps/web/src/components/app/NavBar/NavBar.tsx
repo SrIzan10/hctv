2a15a6367a4b8df8256f95e4a946f5163d190d78
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-12-03 20:25:21 +0100

    chore: change navbar login icon

--- apps/web/src/components/app/NavBar/NavBar.tsx
+++ apps/web/src/components/app/NavBar/NavBar.tsx
@@ -15,7 +15,7 @@ import { logout } from '@/lib/auth/actions';
 import { useSession } from '@/lib/providers/SessionProvider';
 import Link from 'next/link';
 import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
-import { Slack } from 'lucide-react';
+import { IdCard, Slack } from 'lucide-react';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 
 export default function Navbar(props: Props) {
@@ -99,7 +99,7 @@ export default function Navbar(props: Props) {
           ) : (
             <Link href="/auth/hackclub">
               <Button variant="outline" size="sm" className="gap-1 md:gap-2 text-xs md:text-sm">
-                <Slack className="w-3 h-3 md:w-4 md:h-4" />
+                <IdCard className="w-3 h-3 md:w-4 md:h-4" />
                 <span className="hidden sm:inline">Sign in</span>
                 <span className="sm:hidden">Login</span>
               </Button>
