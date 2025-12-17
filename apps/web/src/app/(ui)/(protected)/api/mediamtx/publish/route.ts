1ff51fad619dcfc46131b0e426fd7d191f0fc5a5
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-12-17 18:33:21 +0100

    feat: streaminfo and thumbnail wiring

--- apps/web/src/app/(ui)/(protected)/api/mediamtx/publish/route.ts
+++ apps/web/src/app/(ui)/(protected)/api/mediamtx/publish/route.ts
@@ -1,8 +1,10 @@
 import { prisma, getRedisConnection } from '@hctv/db';
 import { NextRequest } from 'next/server';
 import { z } from 'zod';
+import { lucia } from '@hctv/auth';
 
 export async function POST(request: NextRequest) {
+  const redis = getRedisConnection();
   const body = await request.json();
 
   const parsed = schema.safeParse(body);
@@ -12,35 +14,26 @@ export async function POST(request: NextRequest) {
   }
   const { action, protocol, path, password } = parsed.data;
   if (action === 'publish' && protocol === 'srt') {
-    const redis = getRedisConnection();
-    const channelKey = await redis.get(`streamKey:${path}`)
+    const channelKey = await redis.get(`streamKey:${path}`);
 
     if (channelKey) {
       if (channelKey !== password) {
         return new Response('invalid stream key', { status: 403 });
       }
       return new Response('youre in yay', { status: 200 });
-    } else {
-      const key = await prisma.streamKey.findFirst({
-        where: {
-          key: password,
-          channel: {
-            name: path,
-          }
-        },
-        include: {
-          channel: true,
-        },
-      });
-
-      if (!key) {
-        return new Response('invalid stream key', { status: 403 });
-      }
     }
+  } else if (action === 'read' && protocol === 'hls') {
+    if (password === process.env.MEDIAMTX_PUBLISH_KEY) {
+      return new Response('authorized', { status: 200 });
+    }
+    const sessionExists = await redis.exists(`sessions:${password}`);
+    if (!sessionExists) {
+      return new Response('unauthorized', { status: 401 });
+    }
+    return new Response('authorized', { status: 200 });
   }
-  
-  return new Response('Request processed', { status: 200 });
 
+  return new Response('uhh', { status: 401 });
 }
 
 const schema = z.object({
