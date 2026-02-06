0157eff9f325b682c13e3c0a96b656f0bd658e43
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2026-02-06 23:02:25 +0100

    fix: onboarding errors

--- apps/web/src/lib/form/actions.ts
+++ apps/web/src/lib/form/actions.ts
@@ -105,6 +105,15 @@ export async function onboard(prev: any, formData: FormData) {
 
   await generateStreamKey(createdChannel.id, createdChannel.name);
 
+  if (process.env.NODE_ENV === 'production' && process.env.WELCOME_WORKFLOW_URL) {
+    await fetch(process.env.WELCOME_WORKFLOW_URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: zod.data.username,
+      }),
+    });
+  }
+
   return { success: true };
 }
 
